dd1cd02fcfc6886e519d84396e50d3523b57d749
james <kjic5441@gmail.com>
2021-10-25 13:48:29 +0800

    sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cc4j!58jlirh(qx0^*7-*sg!!=&cbs#=ipvolu5p=yd@i-)b@4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['192.168.198.11','127.0.0.1','192.168.124.11','192.168.100.110','192.168.55.137','192.168.1.104']
 ALLOWED_HOSTS = ['*']
@@ -76,17 +76,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'mysite',
+#             'USER':'root',
+#             'PASSWORD':'a123456',
+#             'HOST':'127.0.0.1',
+#             'PORT':3306,
+#     }
+# }
+
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'mysite',
-            'USER':'root',
-            'PASSWORD':'a123456',
-            'HOST':'127.0.0.1',
-            'PORT':3306,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
